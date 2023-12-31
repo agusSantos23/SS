7cd6a983228aafed978156a7e58ec33a0d27a80d
Agustin S Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2023-12-31 19:26:57 +0100

    2024 tiene buena pinta

--- script.js
+++ script.js
@@ -4,10 +4,6 @@
 
 
 
-
-
-
-
 //AnimacionBurguer
 function BurgerA(){
 
@@ -23,28 +19,27 @@ function BurgerA(){
     if(burguer.classList.contains(activado)){
 
         burguer.classList.remove(activado)
+        burguer.style.transition = "1s"
         burguer.style.transform = "rotate(0deg)";
 
 
         setTimeout(() => {
             aa.style.opacity = "0";
             aa.style.transition = ".5s"
-        }, 300);
-
-        setTimeout(() => {
+            aa.style.fontSize = "0px";
             ab.style.opacity = "0";
             ab.style.transition = ".5s"
-        }, 600);
-
-        setTimeout(() => {
+            ab.style.fontSize = "0px";
             ac.style.opacity = "0";
             ac.style.transition = ".5s"
-        }, 900);
-
-        setTimeout(() => {
+            ac.style.fontSize = "0px";
             ad.style.opacity = "0";
             ad.style.transition = ".5s"
-        }, 1200);
+            ad.style.fontSize = "0px";
+
+        }, 300);
+
+        
         
     }else{
 
@@ -55,28 +50,36 @@ function BurgerA(){
         setTimeout(() => {
             aa.style.opacity = "1";
             aa.style.transition = "1s"
-        }, 1200);
+            aa.style.fontSize = "20px"
+        }, 600);
 
         setTimeout(() => {
             ab.style.opacity = "1";
-            ab.style.transition = "1s"
-        }, 900);
+            ab.style.transition = "1s";
+            ab.style.fontSize = "20px"
+
+        }, 400);
 
         setTimeout(() => {
             ac.style.opacity = "1";
             ac.style.transition = "1s"
-        }, 600);
+            ac.style.fontSize = "20px"
+
+        }, 200);
 
         setTimeout(() => {
             ad.style.opacity = "1";
             ad.style.transition = "1s"
-        }, 300);
+            ad.style.fontSize = "20px"
+
+        }, 0);
 
     }
     
         
 
 }
+
 
 //IconGmail
 function ActivarCA(){
