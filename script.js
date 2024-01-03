171fb3ad2a26163cc1cc6249b65e119236b84323
Agustin S Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-01-03 23:36:26 +0100

    1.1

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function BurgerA(){
 
     const burguer = document.getElementById("burguer");
     
+    const en = document.getElementById("enlaces");
+
     const aa = document.getElementById("Inici");
     const ab = document.getElementById("Sobre");
     const ac = document.getElementById("Edu");
@@ -27,16 +29,28 @@ function BurgerA(){
             aa.style.opacity = "0";
             aa.style.transition = ".5s"
             aa.style.fontSize = "0px";
+            aa.style.margin = "0px";
+            aa.style.padding = "0px";
+
             ab.style.opacity = "0";
             ab.style.transition = ".5s"
             ab.style.fontSize = "0px";
+            ab.style.margin = "0px";
+            ab.style.padding = "0px";
+
             ac.style.opacity = "0";
             ac.style.transition = ".5s"
             ac.style.fontSize = "0px";
+            ac.style.margin = "0px";
+            ac.style.padding = "0px";
+
             ad.style.opacity = "0";
             ad.style.transition = ".5s"
             ad.style.fontSize = "0px";
+            ad.style.margin = "0px";
+            ad.style.padding = "0px";
 
+            
         }, 300);
 
         
@@ -51,28 +65,29 @@ function BurgerA(){
             aa.style.opacity = "1";
             aa.style.transition = "0.5s"
             aa.style.fontSize = "20px"
-        }, 150);
-
-        setTimeout(() => {
+            aa.style.margin = "0 5px";
+            aa.style.padding = "5px";
+        
             ab.style.opacity = "1";
             ab.style.transition = "0.5s";
             ab.style.fontSize = "20px"
-
-        }, 100);
-
-        setTimeout(() => {
+            ab.style.margin = "0 5px";
+            ab.style.padding = "5px";
+        
             ac.style.opacity = "1";
             ac.style.transition = "0.5s"
             ac.style.fontSize = "20px"
-
-        }, 50);
-
-        setTimeout(() => {
+            ac.style.margin = "0 20px 0 0";
+            ac.style.padding = "5px";
+        
             ad.style.opacity = "1";
             ad.style.transition = "0.5s"
             ad.style.fontSize = "20px"
-
-        }, 0);
+            ad.style.margin = "0 5px";
+            ad.style.padding = "5px";
+            
+            
+        }, 100);
 
     }
     
